8affbc19bbe5378e5fff67a4f74111d9cb70f4c7
ZhekaLited <94718651+ZhekaLited@users.noreply.github.com>
2022-04-16 18:17:08 +0300

    Version 0.1

---
+++ src/main/java/com/itvdn/junit/Numbers.java
@@ -0,0 +1,12 @@
+package com.itvdn.junit;
+
+public class Numbers {
+
+    public int sum(int Numbers) {
+        int result = 0;
+        for (int i = 0; i <= Numbers; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/itvdn/junit/Program.java
@@ -0,0 +1,10 @@
+package com.itvdn.junit;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Numbers numbers = new Numbers();
+        int sum = numbers.sum(6);
+        System.out.println("Sum from 0 till 5 = " + sum);
+    }
+}
